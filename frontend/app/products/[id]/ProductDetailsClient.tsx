4f07bf1748b0203d17f8c55201a4563b9717194b
Ghox19 <gonzalo.moncada19@gmail.com>
2025-11-13 19:56:24 -0300

    chore: space y in detail product

--- frontend/app/products/[id]/ProductDetailsClient.tsx
+++ frontend/app/products/[id]/ProductDetailsClient.tsx
@@ -180,7 +180,7 @@ export default function ProductDetailsClient({ product, variations }: bruh) {
   return (
     <div className="w-[85vw] mx-auto">
       {/* TODO: IMPLEMENTAR BREADCRUMBS PARA MANEJO DE CATEGORÍAS */}
-      <p>{`Productos > ${product.categories[0].name} > ${product.name}`}</p>
+      <p className="mb-4">{`Productos > ${product.categories[0].name} > ${product.name}`}</p>
       <div className="grid grid-cols-2">
         <img
           src={currentVariation?.image?.src || product.images[0]?.src}
