11703bb6821b7f53ef2b856b83475f980c996ea4
unknown <ignacioladal@gmail.com>
2026-01-12 02:08:02 -0300

    feat: implementa store api en vista de carrito

--- frontend/app/cart/page.tsx
+++ frontend/app/cart/page.tsx
@@ -1,27 +1,130 @@
 "use client";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { setCookie } from 'cookies-next';
 import { useCart } from "../context/CartContext";
 import { useDeliveryAvailability } from "../hooks/useDeliveryAvailability";
 import CartItemRow from "../context/CartItemRow";
+import { useEffect, useState } from "react";
+import DeliveryDateSelector from "../components/DeliveryDateSelector";
+import { useDeliveryDate} from "../context/DeliveryDateContext"
 
 export default function CartPage() {
   const router = useRouter();
-  const { cart, removeFromCart, clearCart, updateQuantity } = useCart();
+  const [cart, setCart] = useState<any>(null);
+  const [fechasDisponibles, setFechasDisponibles] = useState(null)
+  const [selectedDate, setSelectedDate] = useState<string | null>(null);
+  const [maxProductsPerDay, setMaxProductsPerDay] = useState(null)
+  const [totalQuantity, setTotalQuantity] = useState(0)
+
+  
 
   // 1. LLAMAMOS AL HOOK AQUÍ (Una sola vez para toda la página)
   const { data, getDailyRemaining, loading } = useDeliveryAvailability();
 
-  const total = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
 
-  // Opcional: Mostrar loading mientras carga la disponibilidad
-  if (loading) return <div className="p-10 text-center">Cargando disponibilidad...</div>;
+
+  useEffect(() => {
+    (async () => {
+      const res = await fetch("/api/store/cart", { cache: "no-store" });
+      const storeCart = await res.json();
+      setCart(storeCart);
+
+      
+      const resFechas = await fetch('/api/deliveryDates', {
+        method: 'GET'
+      })
+      
+      const fechas = await resFechas.json()
+      setFechasDisponibles(fechas)
+      
+      const resLimite = await fetch("/api/deliveryDates/getLimit", { cache: "no-store" });
+      const limite = await resLimite.json()
+      setMaxProductsPerDay(limite.daily_limit)
+      calculateTotalQuantity(storeCart);
+
+    })();
+  }, []);
+
+
+  const calculateTotalQuantity = (cartData: any) => {
+    const quantity = cartData.items.reduce((total: number, item: any) => {
+      // Each item object has a 'quantity' field (number)
+      return total + item.quantity;
+    }, 0);
+    setTotalQuantity(quantity)
+  }
+
+
+
+  const decreaseQuantity = async (key: string, currentQuantity: number) => {
+      if (currentQuantity == 1) return;
+
+      const res = await fetch("/api/store/cart/updateItem", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          key: key,
+          quantity: currentQuantity - 1,
+        }),
+      });
+      const storeCart = await res.json();
+      setCart(storeCart);
+      calculateTotalQuantity(storeCart);
+    };
+
+  const increaseQuantity = async (key: string, currentQuantity: number) => {
+
+      const res = await fetch("/api/store/cart/updateItem", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          key: key,
+          quantity: currentQuantity + 1,
+        }),
+      });
+      const storeCart = await res.json();
+      setCart(storeCart);
+      calculateTotalQuantity(storeCart);
+    };
+
+    const deleteItem = async (key: string) => {
+
+      const res = await fetch("/api/store/cart/removeItem", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          key: key,
+        }),
+      });
+      const storeCart = await res.json();
+      setCart(storeCart);
+      calculateTotalQuantity(storeCart);
+    };
+
+
+
+  const handleSelectDate = async (date: string, timestamp: string) => {
+    const cookieValue = JSON.stringify({
+        date: date,
+        timestamp: timestamp
+    });
+    setCookie('delivery_selection', cookieValue, { maxAge: 60 * 60 * 24 });
+  }
+
+  const deleteCart = async () => {
+    const res = await fetch("/api/store/cart/newCart", { cache: "no-store" });
+    const storeCart = await res.json();
+    setCart(storeCart);
+  }
+
+  if (!cart) return <div className="p-10 text-center">Cargando carrito...</div>;
 
   return (
     <main className="w-[50%] p-16 mx-auto p-getOrder6">
       <h1 className="text-3xl font-bold mb-8">Tu Carrito</h1>
 
-      {cart.length === 0 ? (
+      {!cart && cart.items.length === 0 ? (
         <div className="text-center mb-24">
           <p className="text-gray-600 mb-6">Tu carrito está vacío 🛍️</p>
           <Link
@@ -33,107 +136,58 @@ export default function CartPage() {
         </div>
       ) : (
         <>
-          {/* <ul className="space-y-4">
-            {cart.map((item) => (
-              <li
-                key={item.id}
-                className="flex items-center justify-between border rounded-xl p-4 shadow-sm bg-white"
-              >
-                <div className="flex items-center gap-4">
-                  {item.image && (
-                    <img
-                      src={item.image}
-                      alt={item.name}
-                      className="w-20 h-20 object-cover rounded-lg"
-                    />
-                  )}
-                  <div>
-                    <h2 className="font-semibold">{item.name}</h2>
-                    <p className="text-gray-600">${item.price}</p>
-                    <p className="text-gray-600">{`Para entrega el día ${item.deliveryDate}`}</p>
-                  </div>
-                </div>
-
-                <div className="flex items-center gap-3">
-                  <button
-                    onClick={() =>
-                      updateQuantity(item.id, Math.max(item.quantity - 1, 1))
-                    }
-                    className="px-3 py-1 border rounded hover:bg-gray-100"
-                  >
-                    −
-                  </button>
-                  <span>{item.quantity}</span>
-                  <button
-                    onClick={() => updateQuantity(item.id, item.quantity + 1)}
-                    className="px-3 py-1 border rounded hover:bg-gray-100"
-                  >
-                    +
-                  </button>
-                </div>
-
-                <div className="text-right">
-                  <p className="font-semibold">
-                    ${(item.price * item.quantity).toFixed(2)}
-                  </p>
-                  <button
-                    onClick={() => removeFromCart(item.id)}
-                    className="text-red-500 hover:underline text-sm"
-                  >
-                    Eliminar
-                  </button>
-                </div>
-              </li>
-            ))}
-          </ul> */}
+          <p>Considera que la pastelería de Mónica tiene un límite de {maxProductsPerDay} productos por día. Considera reducir el tamaño de tu carrito o solicitar un pedido personalizado al Whatsapp</p>
           <ul className="space-y-4">
-            {cart.map((item) => (
+            {cart && cart.items.map((item: any) => (
               // 2. RENDERIZAMOS LA FILA CON LA DATA DEL HOOK
-              <CartItemRow
-                key={`${item.id}-${item.deliveryDate}`} // Key única
+              <CartItemRow // Key única
                 item={item}
-                updateQuantity={updateQuantity}
-                removeFromCart={removeFromCart}
-                // Pasamos las herramientas de validación
-                getDailyRemaining={getDailyRemaining}
-                globalRemaining={data?.global_remaining ?? 9999} // Valor alto por defecto si no ha cargado
+                decreaseQuantity={decreaseQuantity}
+                increaseQuantity={increaseQuantity}
+                deleteItem={deleteItem}// Valor alto por defecto si no ha cargado
               />
             ))}
           </ul>
 
           {/* Footer del carrito */}
           <div className="mt-8 border-t pt-8">
+            {fechasDisponibles && <DeliveryDateSelector
+              availableDates={fechasDisponibles}
+              cartQuantity={totalQuantity}
+              selectedDate={''}
+              onDateSelect={handleSelectDate}
+            />}
             <div className="flex justify-between items-start">
-                <button
-                onClick={clearCart}
+              <button
+                onClick={deleteCart}
                 className="text-gray-400 hover:text-red-500 text-sm transition-colors mt-2"
-                >
+              >
                 Vaciar carrito
+              </button>
+
+              <div className="flex flex-col items-end gap-4 w-full max-w-md">
+                <div className="flex justify-between w-full text-xl">
+                  <span className="text-gray-600">Total:</span>
+                  <span className="font-bold text-2xl">${cart.totals.total_items}</span>
+                </div>
+
+                {/* Botón de pago */}
+                {/* Podrías deshabilitarlo si loading es true */}
+                <button
+                  onClick={() => router.push("/checkout")}
+                  disabled={loading}
+                  className="w-[50%]  bg-[#E985A7] text-white px-6 py-3 rounded-4xl font-semibold text-lg shadow-lg hover:shadow-[#E985A7]/40 hover:bg-[#d96b8f] hover:scale-[1.02] transition-all disabled:opacity-50"
+                >
+                  {"Ir al pago →"}
                 </button>
 
-                <div className="flex flex-col items-end gap-4 w-full max-w-md">
-                    <div className="flex justify-between w-full text-xl">
-                        <span className="text-gray-600">Total:</span>
-                        <span className="font-bold text-2xl">${total.toFixed(2)}</span>
-                    </div>
-                    
-                    {/* Botón de pago */}
-                    {/* Podrías deshabilitarlo si loading es true */}
-                    <button
-                        onClick={() => router.push("/checkout")}
-                        disabled={loading}
-                        className="w-[50%]  bg-[#E985A7] text-white px-6 py-3 rounded-4xl font-semibold text-lg shadow-lg hover:shadow-[#E985A7]/40 hover:bg-[#d96b8f] hover:scale-[1.02] transition-all disabled:opacity-50"
-                    >
-                        {loading ? "Verificando disponibilidad..." : "Ir al pago →"}
-                    </button>
-                    
-                    <button 
-                        onClick={() => router.push("/products")}
-                        className="text-pink-500 hover:text-pink-700 text-sm font-medium"
-                    >
-                        Seguir comprando
-                    </button>
-                </div>
+                <button
+                  onClick={() => router.push("/products")}
+                  className="text-pink-500 hover:text-pink-700 text-sm font-medium"
+                >
+                  Seguir comprando
+                </button>
+              </div>
             </div>
           </div>
 
